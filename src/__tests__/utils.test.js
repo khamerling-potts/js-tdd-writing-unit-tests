8cd7f923f3d030cc3722edfb10eebea910e65d36
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-23 13:37:43 -0400

    initial test passing

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,10 @@
 // Your tests here
+import { pointsForWords } from "../utils";
+
+describe("pointsForWords", () => {
+  it("calculates the total points for a word (1 per vowel, 2 per consonant)", () => {
+    const word = "test";
+    const points = pointsForWords(word);
+    expect(points).toBe(7);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export function pointsForWords(word) {
+  let points = 0;
+  for (const char of word) {
+    if (["a", "e", "o", "i", "u"].includes(char)) {
+      points += 1;
+    } else {
+      points += 2;
+    }
+  }
+  return points;
+}
